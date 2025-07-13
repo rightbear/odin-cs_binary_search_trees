f385a55eea0409bf7a82f688263232ae7964280d
rightbear <jjj17041113@gmail.com>
2025-07-13 21:48:40 +0800

    feat: test buildTree, sortedArrayToBSTRecur, prettyPrint functions in main.js

---
+++ functions/main.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+import { Tree } from "./classes.js"
+
+let testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+let BSTExample = new Tree(testArray);
+
+// test function prettyPrint()
+console.log(BSTExample.prettyPrint(BSTExample.root));
