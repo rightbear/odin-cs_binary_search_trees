6bda75b878dafb9bb0f7b352817ffd164235fcc0
rightbear <jjj17041113@gmail.com>
2025-07-14 17:14:38 +0800

    test: test find, two versions of levelOrderForEach, two versions of callback in main.js

--- functions/main.js
+++ functions/main.js
@@ -26,3 +26,43 @@ BSTExample.prettyPrint(BSTExample.root);
 // test function delete() with value having 2 child
 BSTExample.deleteItem(67);
 BSTExample.prettyPrint(BSTExample.root);
+
+// test function find() with non-existed value
+console.log(BSTExample.find(77));
+// test function find()
+console.log(BSTExample.find(8));
+console.log(BSTExample.find(324));
+console.log(BSTExample.find(3));
+
+
+// test function levelOrderForEach()
+try {
+    BSTExample.levelOrderForEach(BSTExample.accumValueCallbackRecur);
+} catch (error) {
+    console.error('Caught an error:', error.message);
+}
+// 8, 4, 250, 3, 5, 9, 324, 7, 6345
+
+// test function levelOrderForEach() without callback
+try {
+    BSTExample.levelOrderForEach();
+} catch (error) {
+    console.error('Caught an error:', error.message);
+}
+// Caught an error: Callback function is required.
+
+// test function levelOrderForEachIter()
+try {
+    BSTExample.levelOrderForEachIter(BSTExample.accumValueCallbackIter);
+} catch (error) {
+    console.error('Caught an error:', error.message);
+}
+// 8, 4, 250, 3, 5, 9, 324, 7, 6345
+
+// test function levelOrderForEachIter() without callback
+try {
+    BSTExample.levelOrderForEachIter();
+} catch (error) {
+    console.error('Caught an error:', error.message);
+}
+// Caught an error: Callback function is required.
